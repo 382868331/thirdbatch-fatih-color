beb479e9bcea1fe31d8a31c716e0899d368630c3
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:31:37 +0800

    fix: restore 资源清理从延迟执行改为立即执行 behavior

--- color.go
+++ color.go
@@ -309,7 +309,7 @@ func (c *Color) Fprint(w io.Writer, a ...interface{}) (n int, err error) {
 // color.
 func (c *Color) Print(a ...interface{}) (n int, err error) {
 	c.Set()
-	c.unset()
+	defer c.unset()
 
 	return fmt.Fprint(Output, a...)
 }
